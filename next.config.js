128090f59f0816c1f0dae79f04d49573f3bba745
Daniyal <daniyalhayat10@gmail.com>
2026-03-29 16:26:37 +0500

    Fix build config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['avatars.githubusercontent.com','lh3.googleusercontent.com'] },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  }
 }
 module.exports = nextConfig
